1176271843e7bba41f7907abb085de65882c3450
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-02-16 14:30:48 +0000

    Add when destroy to null_resource

--- main.tf
+++ main.tf
@@ -4,4 +4,8 @@ resource "null_resource" "ns_role_annotation" {
   provisioner "local-exec" {
     command = "kubectl annotate namespace ${var.namespace} 'iam.amazonaws.com/permitted=${join("|", [for role in var.ns_annotation_roles : format("%s", role)])}' --overwrite"
   }
+   provisioner "local-exec" {
+    when = destroy
+    command = "kubectl annotate namespace ${var.namespace} 'iam.amazonaws.com/permitted-'"
+  }
 }
